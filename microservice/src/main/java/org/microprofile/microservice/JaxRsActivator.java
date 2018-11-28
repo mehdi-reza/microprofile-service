7b1a7d4c0e31e5bde9847b0396ad812168b12ade
mehdi@venturedive.com <mehdi@venturedive.com>
2018-11-28 12:48:49 +0500

    first commit

---
+++ microservice/src/main/java/org/microprofile/microservice/JaxRsActivator.java
@@ -0,0 +1,22 @@
+package org.microprofile.microservice;
+
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JaxRsActivator extends Application {
+	
+	@Override
+	public Set<Class<?>> getClasses() {
+		super.getClasses().add(AbstractMicroService.class);
+		return super.getClasses();
+	}
+	
+	@PostConstruct
+	public void initialize() {
+		System.out.println("Initialized");
+	}
+}
